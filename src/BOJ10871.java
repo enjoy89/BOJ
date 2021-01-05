1798854e87ee4730d60b4da217ae513098d80ccf
Jeon Dui <jeondui@gmail.com>
2021-01-06 01:12:40 +0900

    init

---
+++ src/BOJ10871.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int num1 = Integer.parseInt(s[0]);
+        int num2 = Integer.parseInt(s[1]);
+
+        String[] arr = br.readLine().split(" ");
+        for (int i = 0; i < num1; i++) {
+            if (Integer.parseInt(arr[i]) < num2) {
+                System.out.print(Integer.parseInt(arr[i]) + " ");
+            }
+        }
+    }
+}
