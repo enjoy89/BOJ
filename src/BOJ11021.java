71966f9d895c47489ab3bff7dc783e091c788533
Jeon Dui <jeondui@gmail.com>
2021-02-08 19:38:42 +0900

    init

---
+++ src/BOJ11021.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ11021 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int result = 0;
+
+        for(int i=0; i<n; i++) {
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            result = num1 + num2;
+            System.out.println("Case #" + (i+1) + ": " + num1 + " + " + num2 + " = " + result);
+        }
+
+    }
+}
---
+++ src/BOJ11022.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ11022 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int result = 0;
+
+        for(int i=0; i<n; i++) {
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            result = num1 + num2;
+            System.out.println("Case #" + (i+1) + ": " + num1 + " + " + num2 + " = " + result);
+        }
+    }
+}
