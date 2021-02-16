d5cf5b02eeeb13277e48ec43a98bcf2f9cde3a08
Jeon Dui <jeondui@gmail.com>
2021-02-17 02:12:45 +0900

    init

---
+++ src/BOJ10872.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10872 {
+
+    public static int factorial(int N) {
+        if (N <= 1) {
+            return 1;
+        } else {
+            return N * factorial(N - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = factorial(N);
+        System.out.println(result);
+
+    }
+}
