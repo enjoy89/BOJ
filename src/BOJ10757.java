b83bf048097288e3fdd2962283fa1d22a00dc675
Jeon Dui <jeondui@gmail.com>
2021-02-17 01:53:29 +0900

    init

---
+++ src/BOJ10757.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BOJ10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        System.out.println(new BigInteger(arr[0]).add(new BigInteger(arr[1])));
+    }
+}
