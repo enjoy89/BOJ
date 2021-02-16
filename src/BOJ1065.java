69e44b5673b0647c0995b72776308bb451b0db3d
Jeon Dui <jeondui@gmail.com>
2021-02-17 01:46:53 +0900

    init

---
+++ src/BOJ1065.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1065 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int han = 0;
+
+        if (N < 100) {
+            han = N;
+        } else {
+            han = 99;
+
+            if (N == 1000) {
+                N = 999;
+            }
+            for (int i = 100; i <= N; i++) {
+                int num1 = i / 100;
+                int num2 = (i / 10) % 10;
+                int num3 = i % 10;
+
+                if ((num1 - num2) == (num2 - num3)) {
+                    han++;
+                }
+            }
+        }
+        System.out.println(han);
+    }
+}
