1aa36f4012492e7c8a3b7de9501c23df7c8f2ead
Jeon Dui <jeondui@gmail.com>
2021-02-16 20:50:17 +0900

    init

---
+++ src/BOJ1181.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> list = new ArrayList<>(set);
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return Integer.compare(o1.length(), o2.length());
+                }
+            }
+        });
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
