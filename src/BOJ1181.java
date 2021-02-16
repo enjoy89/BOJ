a11cec3b96f6d18b28fb263de1c499b0ea2caca4
Jeon Dui <jeondui@gmail.com>
2021-02-17 00:48:01 +0900

    init

--- src/BOJ1181.java
+++ src/BOJ1181.java
@@ -7,14 +7,17 @@ public class BOJ1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        HashSet<String> set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();  // 단어의 중복을 제거하기 위해 HashSet이용
 
         for (int i = 0; i < N; i++) {
             set.add(br.readLine());
         }
 
-        ArrayList<String> list = new ArrayList<>(set);
+        ArrayList<String> list = new ArrayList<>(set);  // ArrayList로 변환
 
+        // 리스트를 조건에 따라 정렬한다.
+        // 단어의 길이가 같을 경우 사전 순으로 정렬한다.
+        // 단어의 길이가 다를 경우 길이가 짧은 것부터 정렬한다.
         Collections.sort(list, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
