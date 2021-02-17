a7431b50d795a2eaf95b7959900fff05287f43b5
Jeon Dui <jeondui@gmail.com>
2021-02-17 18:18:15 +0900

    init

---
+++ src/BOJ2675.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");    // String 배열에 공백 기준으로 나눠 저장한다.
+            int R = Integer.parseInt(str[0]);
+            String S = str[1];
+
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    System.out.print(S.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
