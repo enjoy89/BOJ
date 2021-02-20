fdf719909ea20015690211cea772cfacbff8c65a
Jeon Dui <jeondui@gmail.com>
2021-02-21 01:32:55 +0900

    init

---
+++ src/BOJ10828.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10828 {
+    public static int[] stack;
+    public static int top;
+    public static int size;
+
+    public static void push(int data) {
+        stack[++top] = data;
+        size++;
+
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int value = stack[top];
+            stack[top--] = 0;
+            size--;
+            return value;
+
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[top];
+        }
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int size() {
+        return size;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        stack = new int[N];
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch (st.nextToken()) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    System.out.println(pop());
+                    break;
+                case "size":
+                    System.out.println(size());
+                    break;
+                case "empty":
+                    System.out.println(empty());
+                    break;
+                case "top":
+                    System.out.println(top());
+                    break;
+            }
+        }
+    }
+}
