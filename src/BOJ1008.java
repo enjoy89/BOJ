52f64b4f474cc12456ac6265e30706b2a2b90925
Jeon Dui <jeondui@gmail.com>
2021-02-24 16:16:11 +0900

    init

---
+++ src/BOJ1008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.println(a/b);
+    }
+}
