539fda6c8de421b33a2fa0a0e7f13ee7d3b76324
jeondui <jeondui@gmail.com>
2021-07-22 22:21:30 +0900

    두 수 비교하기

--- src/BOJ1330.java
+++ src/BOJ1330.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ1330 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num1 = input.nextInt();
-        int num2 = input.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[1]);
 
         if (num1 > num2) {
             System.out.println(">");
