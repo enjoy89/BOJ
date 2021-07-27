865af5620ec35edfae3e8537048ec606403debd0
jeondui <jeondui@gmail.com>
2021-07-27 13:54:25 +0900

    수 정렬하기 2

--- src/BOJ2751.java
+++ src/BOJ2751.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class BOJ2751 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int N = input.nextInt();
         ArrayList<Integer> nums = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            nums.add(input.nextInt());
+            nums.add(Integer.parseInt(br.readLine()));
         }
         Collections.sort(nums);
 
