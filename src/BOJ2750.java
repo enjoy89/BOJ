610933c184132fdbf6d0571a2caf474000809c68
jeondui <jeondui@gmail.com>
2021-07-27 13:51:19 +0900

    수 정렬하기

--- src/BOJ2750.java
+++ src/BOJ2750.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ2750 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        int[] arr = new int[N];
-        int temp;
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = input.nextInt();
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.println(i);
         }
+    }
 
-        // bubble sort
+    public static int[] Bubble_Sort(int[] arr) {
+        int temp;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -21,9 +20,17 @@ public class BOJ2750 {
                 }
             }
         }
+        return arr;
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        printArray(Bubble_Sort(arr));
     }
 }
