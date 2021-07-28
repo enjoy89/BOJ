aadccef60ec16d1a27431347043a69a5981a49e5
jeondui <jeondui@gmail.com>
2021-07-28 17:32:51 +0900

    A+B-7

--- src/BOJ11021.java
+++ src/BOJ11021.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ11021 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int result = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            int num1 = input.nextInt();
-            int num2 = input.nextInt();
-            result = num1 + num2;
-            System.out.println("Case #" + (i + 1) + ": " + num1 + " + " + num2 + " = " + result);
+        for (int i = 0; i < N; i++) {
+            String[] nums = br.readLine().split(" ");
+            int result = Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+            System.out.println("Case #" + (i + 1) + ": " + result);
         }
-
     }
 }
