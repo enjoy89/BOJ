414216e3c69936a2f8994317960a535aec963b3b
jeondui <jeondui@gmail.com>
2021-07-29 23:52:51 +0900

    사분면 고르기

--- src/BOJ14681.java
+++ src/BOJ14681.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ14681 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n1 = input.nextInt();
-        int n2 = input.nextInt();
-
-        if (n1 > 0 && n2 > 0) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        if (x > 0 && y > 0) {
             System.out.println("1");
-        } else if (n1 > 0 && n2 < 0) {
+        } else if (x > 0 && y < 0) {
             System.out.println("4");
-        } else if (n1 < 0 && n2 > 0) {
+        } else if (x < 0 && y > 0) {
             System.out.println("2");
         } else {
             System.out.println("3");
