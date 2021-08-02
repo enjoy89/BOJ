d24aa215f834969f7529ab936eacca9ca87ceb82
jeondui <jeondui@gmail.com>
2021-08-02 23:53:26 +0900

    좌표 정렬하기2

--- src/BOJ11651.java
+++ src/BOJ11651.java
@@ -12,13 +12,13 @@ public class BOJ11651 {
         int[][] arr = new int[N][2];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");   // 문자열을 공백 기준으로 나눈다. 토큰이라고 부름
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");   // 문자열을 공백 기준으로 나눈다. 이를 토큰이라고 부름
             arr[i][0] = Integer.parseInt(st.nextToken());   // 다음 토큰을 반환
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // 1번째 요소와 다르면 0번째 요소끼리 비교한다.
-        // 1번째 요소와 같으면 1번재 요소끼리 비교한다.
+        // 1번째 요소와 다르면 0번째 요소끼리 비교
+        // 1번째 요소와 같으면 1번재 요소끼리 비교
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -30,9 +30,9 @@ public class BOJ11651 {
             }
         });
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int[] ints : arr) {
             for (int j = 0; j < 2; j++) {
-                System.out.print(arr[i][j] + " ");
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
