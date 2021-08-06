b884822fc99a2af2463839f3e3c714aa26e11509
jeondui <jeondui@gmail.com>
2021-08-06 12:39:36 +0900

    ATM

--- src/BOJ11399.java
+++ src/BOJ11399.java
@@ -1,24 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BOJ11399 {
-    public static int[] Insertion_sort(int[] arr) {
-        int temp;
-        for (int i = 1; i < arr.length; i++) {
-            int key = i;
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[key] < arr[j]) {
-                    temp = arr[key];
-                    arr[key] = arr[j];
-                    arr[j] = temp;
-                    key = j;
-                }
-            }
-        }
-        return arr;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -28,11 +13,10 @@ public class BOJ11399 {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(str[i]);
         }
+        Arrays.sort(arr);
+        int sum = arr[0];
 
-        int[] sort_arr = Insertion_sort(arr);
-        int sum = sort_arr[0];
-
-        for (int i = 1; i < sort_arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             arr[i] = arr[i] + arr[i - 1];
             sum += arr[i];
         }
