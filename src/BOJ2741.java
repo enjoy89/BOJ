bff27b5b45a31669f13ea2ae0b0177022983e82b
jeondui <jeondui@gmail.com>
2021-08-08 20:03:51 +0900

    N 찍기

--- src/BOJ2741.java
+++ src/BOJ2741.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ2741 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-
-        for (int i = 1; i <= n; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
             System.out.println(i);
         }
     }
-
 }
