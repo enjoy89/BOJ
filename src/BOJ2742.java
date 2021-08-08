c09dc453331065c2077cc0f814dc8c1d1cc2e84d
jeondui <jeondui@gmail.com>
2021-08-08 20:06:08 +0900

    기찍 N

--- src/BOJ2742.java
+++ src/BOJ2742.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ2742 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = n; i >= 1; i--) {
+        for (int i = N; i >= 1; i--) {
             System.out.println(i);
         }
     }
