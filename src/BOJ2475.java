69e112b017afc12ea280462d9e54cdf2071ba721
jeondui <jeondui@gmail.com>
2021-08-10 20:15:16 +0900

    검증수

--- src/BOJ2475.java
+++ src/BOJ2475.java
@@ -7,13 +7,15 @@ public class BOJ2475 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
-        int[] nums = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        int[] nums = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            nums[i] = Integer.parseInt(str[i]);
+        }
         int result = 0;
 
-        for(int i =0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             nums[i] *= nums[i];
             result += nums[i];
-            System.out.println(nums[i]);
         }
         System.out.println(result % 10);
     }
