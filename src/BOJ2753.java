9decff72383573a423be07c0e130a6627e667192
jeondui <jeondui@gmail.com>
2021-08-19 18:41:00 +0900

    윤년

--- src/BOJ2753.java
+++ src/BOJ2753.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BOJ2753 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+        if ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0) {
             System.out.println("1");
         } else
             System.out.println("0");
