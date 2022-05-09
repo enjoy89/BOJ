d7b856e13e27c8f0694533650f60e8f7edc20ccb
jeondui <jeondui@gmail.com>
2022-05-09 18:05:07 +0900

    동전 0

--- src/BOJ11047.java
+++ src/BOJ11047.java
@@ -1,29 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ11047 {
     public static void main(String[] args) throws IOException {
-        // 그리디 알고리즘
-        // N개의 동전 중 가장 큰 가치를 지닌 동전부터 탐색한다.
-        // 동전의 가치가 K보다 클 경우에는 패스, 아닐 경우 최대 구성 가능한 개수를 더해줌
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] coin = new int[N];
+        String[] nums = br.readLine().split(" ");
+        int N = Integer.parseInt(nums[0]);  // 동전 종류의 개수
+        int K = Integer.parseInt(nums[1]);  // 동전 가치의 합
         int count = 0;
 
+        // 동전의 가치를 입력 받아 정수형 배열로 구성
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 가장 큰 가치를 지닌 동전부터 탐색함
-        for (int i = coin.length - 1; i >= 0; i--) {
-            if (coin[i] <= K) {
-                count += (K / coin[i]);
-                K %= coin[i];
+        // 가치가 큰 동전부터 비교해야하므로 배열의 끝에서부터 시작함
+        // 동전보다 K의 값이 크면 해당 동전의 가치로 나눈 몫을 count에 더해주고 나머지 값을 K에 저장
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] <= K) {
+                count += (K / arr[i]);
+                K %= arr[i];
             }
         }
         System.out.println(count);
